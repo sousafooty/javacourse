8fe9cb2937a11c7c9d25d31dc22295c2377a355a
Jose Andres <jsousbr7@gmail.com>
2019-05-14 16:15:59 +0200

    adding new text to Documentation

--- src/views/Documentation.vue
+++ src/views/Documentation.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
     <h1>Documentation</h1>
+    <section>
+      <article>
+        <h3>The Most Widely Used Java Libraries</h3>
+        <p>
+        A typical Java project relies on third-party libraries.
+        This page summarizes the most popular and widely used Java libraries for a variety of different applications.
+        A simple example is also provided for some of them, if it can be found on <a href="https://www.programcreek.com/">ProgramCreek.</a></p>
+
+        <p>Java SDK is surely the #1 widely used library. So the focus of this list is the popular third-party libraries.
+        The list may not not perfect.</p>
+
+        <h4>Third Party Libraries</h4>
+
+        <h4>Standard Libraries</h4>
+      </article>
+    </section>
   </div>
 </template>
