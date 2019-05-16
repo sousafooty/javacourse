1abe2facf20ee71342e89e61480e7c3cc9a9d369
Jose Andres <jsousbr7@gmail.com>
2019-05-16 14:37:43 +0200

    showing information in page documentation

--- src/views/Documentation.vue
+++ src/views/Documentation.vue
@@ -3,19 +3,47 @@
     <h1>Documentation</h1>
     <section>
       <article>
-        <h3>The Most Widely Used Java Libraries</h3>
+        <h2>The Most Widely Used Java Libraries</h2>
         <p>
-        A typical Java project relies on third-party libraries.
-        This page summarizes the most popular and widely used Java libraries for a variety of different applications.
-        A simple example is also provided for some of them, if it can be found on <a href="https://www.programcreek.com/">ProgramCreek.</a></p>
+          A typical Java project relies on third-party libraries.
+          This page summarizes the most popular and widely used Java libraries for a variety of different applications.
+          A simple example is also provided for some of them, if it can be found on
+          <a href="https://www.programcreek.com/">ProgramCreek.</a>
+        </p>
 
-        <p>Java SDK is surely the #1 widely used library. So the focus of this list is the popular third-party libraries.
-        The list may not not perfect.</p>
+        <p>
+          Java SDK is surely the #1 widely used library. So the focus of this list is the
+          <strong>most popular</strong> libraries.
+          The list may not not perfect.
+        </p>
 
-        <h4>Third Party Libraries</h4>
+        <h3>Third Party Libraries</h3>
+        <ol>
+          <li v-for="library in getStandardLibraries" v-bind:key="library.id">
+            <code>{{ library.library }}</code>
+          </li>
+        </ol>
 
-        <h4>Standard Libraries</h4>
+        <h3>Standard Libraries</h3>
+        <ol>
+          <li v-for="externalLibrary in getExternalLibraries" v-bind:key="externalLibrary.id">
+            <code>{{ externalLibrary.library }}</code>
+          </li>
+        </ol>
       </article>
     </section>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    getStandardLibraries () {
+      return this.$store.getters.getStandardLibraries
+    },
+    getExternalLibraries () {
+      return this.$store.getters.getExternalLibraries
+    }
+  }
+}
+</script>
