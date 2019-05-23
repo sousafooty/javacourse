31851242db3d252667c11ffe9d9e9cbd452e37ca
Jose Andres <jsousbr7@gmail.com>
2019-05-23 11:28:41 +0200

    adding styles boostrap

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <b-navbar toggleable="lg" type="dark" variant="dark" fixed="top">
+      <b-navbar-brand to="/">Java Course</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-container>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item to="/Documentation">Documentation</b-nav-item>
+            <b-nav-item to="/Course">Course</b-nav-item>
+            <b-nav-item to="/help">Help</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/Documentation">Documentation</router-link> |
       <router-link to="/Course">Course</router-link> |
       <router-link to="/help">Help</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
@@ -25,9 +39,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 5%;
 }
 #nav {
   padding: 30px;
+  margin-top: 20%;
 }
 
 #nav a {
