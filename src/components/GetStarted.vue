37ffe8ad1210adb7d91a1ff68fb125011c7d33a5
Jose Andres <jsousbr7@gmail.com>
2019-05-29 11:58:41 +0200

    adding buttons to log in

--- src/components/GetStarted.vue
+++ src/components/GetStarted.vue
@@ -4,6 +4,12 @@
       <h1>Java Open Crash Course</h1>
       <h2>Learn how to code in Java</h2>
     </b-container>
+    <b-container>
+      <!--Boton de loguin-->
+      <b-button pill variant="outline-secondary">Google Account
+        <b-badge variant="dark">Get in</b-badge>
+      </b-button>
+    </b-container>
   </div>
 </template>
 
