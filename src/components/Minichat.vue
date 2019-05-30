387021ae445eb0214bf4df634b3be2d99b722e91
Jose Andres <jsousbr7@gmail.com>
2019-05-30 10:43:29 +0200

    implementing chat

---
+++ src/components/Minichat.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-container fluid>
+    <b-row align-h="center">
+      <b-col sm="5">
+        <b-form-textarea
+          id="textarea-small"
+          size="sm"
+          v-model="text"
+          :state="text.length >= 10"
+          placeholder="Your question (at least 10 characters)?"
+        ></b-form-textarea>
+      </b-col>
+      <b-col sm="1">
+        <b-button variant="dark">Send</b-button>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'minichat',
+  data () {
+    return {
+      text: ''
+    }
+  }
+}
+</script>
--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="help">
     <b-container>
-      <h1>Are you lost?</h1>
-      <h3>You think this isn't for you?</h3>
+      <b-row align-h="center">
+        <header>
+          <h1>Are you lost?</h1>
+          <h2>You think this isn't for you?</h2>
+        </header>
+        <Minichat></Minichat>
+      </b-row>
     </b-container>
   </div>
 </template>
+
+<script>
+import Minichat from '@/components/Minichat.vue'
+
+export default {
+  components: {
+    Minichat
+  }
+}
+</script>
